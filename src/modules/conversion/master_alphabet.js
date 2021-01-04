8d99a7c8c72dbee7815d2b96bb5a4fec5c8e3ba8
hadi tarhini <hi@how.hadi.is>
2021-01-03 16:32:14 -0800

    Add eslint overrides, update exports + syntax in master_alphabet.js

--- src/modules/conversion/master_alphabet.js
+++ src/modules/conversion/master_alphabet.js
@@ -1,33 +1,36 @@
 import { readFileSync } from 'fs';
 
-export const letters = {};
+const letters = {};
 let readingLetters = false;
 
-// legit not sure how this path works, like if it 100% has to be from project root or why it can't just be ./grammar.ne
+// legit not sure how this path works, like if it 100% has to be
+// from project root or why it can't just be ./grammar.ne
 const grammarPath = `src/modules/conversion/grammar.ne`;
 
 try {
-    const content = readFileSync(grammarPath, `utf-8`);
-    content.split(/\r?\n/).forEach(line => {
-      if (line.startsWith(`#`) && line.endsWith(`LETTERS #`)) {
-        readingLetters = line.startsWith(`# BEGIN`);
-        return;
+  const content = readFileSync(grammarPath, `utf-8`);
+  content.split(/\r?\n/).forEach(line => {
+    if (line.startsWith(`#`) && line.endsWith(`LETTERS #`)) {
+      readingLetters = line.startsWith(`# BEGIN`);
+      return;
+    }
+    if (!readingLetters) {
+      return;
+    }
+    if (line.trim().startsWith(`#`)) {
+      return;
+    }
+    const match = line.match(/(\w+) -> "(.+?)"/);
+    if (match) {
+      letters[match[1]] = match[2];
+      if (!/^[A-Z]$/.test(match[1][0])) {
+        // as a dot-notation alternative
+        letters[`_${match[1]}`] = match[2];
       }
-      if (!readingLetters) {
-        return;
-      }
-      if (line.trim().startsWith(`#`)) {
-        return;
-      }
-      const match = line.match(/(\w+) -> "(.+?)"/);
-      if (match) {
-        letters[match[1]] = match[2];
-        if (!/^[A-Z]$/.test(match[1][0])) {
-          // as a dot-notation alternative
-          letters[`_${match[1]}`] = match[2];
-        }
-      }
-    });
+    }
+  });
 } catch (e) {
-    console.error(e);
+  console.error(e);
 }
+
+export default letters;
