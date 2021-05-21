0fc6972e1efb31e748a11ed2713508c1288c1f26
hadi tarhini <hi@how.hadi.is>
2021-05-20 18:10:37 -0700

    Add choice-deduplication method

--- src/backend/conversion/objects/choice.js
+++ src/backend/conversion/objects/choice.js
@@ -10,6 +10,17 @@ class Choice {
   ctx(item) {
     this.choices.forEach(obj => obj.ctx(item));
   }
+
+  // remove duplicates based on key func
+  // prob inefficient lmfao
+  dedup(toKey) {
+    const map = {};
+    // not doing fromEntries bc definitely inefficient
+    // and this in turn is less efficient than a for loop but we'll see
+    this.choices.forEach(c => { map[toKey(c)] = c; });
+    /* return new Choice(map.values()); */
+    this.choices = map.values();
+  }
 }
 
 function choice(...objs) {
