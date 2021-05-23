4dbc54edf1979af340f2b6aa62dfafa04d92dd82
hadi tarhini <hi@how.hadi.is>
2021-05-23 03:15:50 -0700

    More floundering in transformer

--- src/backend/conversion/transformers/ipa-phonemic.js
+++ src/backend/conversion/transformers/ipa-phonemic.js
@@ -1,5 +1,6 @@
 /* eslint-disable max-classes-per-file */
 const symbols = require(`../symbols`);
+const {type: objType} = require(`../objs`);
 
 const _ = {
   h: `h`,
@@ -49,7 +50,7 @@ const _ = {
 
   _: `_`,  // no schwa
   Schwa: `'`,
-  Fem: `c`,
+  c: `c`,
   FemPlural: `C`,
   Dual: `=`,
   Plural: `+`,
@@ -88,8 +89,8 @@ class Transformer {
   }
 }
 
-const subAlphabets = (...types) => Object.fromKeys(
-  types.map(
+const subAlphabets = Object.fromKeys(
+  Object.values(objType).map(
     type => [type, Object.values(symbols.alphabet).filter(v => v.type === type)]
   )
 );
@@ -135,24 +136,90 @@ class Props {
   }
 }
 
+const consonantPred = new Props({type: objType.consonant});
+const vowelPred = new Props({type: objType.vowel});
+
 class Cap {
   constructor(word) {
-    this.word = word.value;
-    this.wordMap = mapWord(this.word);
+    this.word = { ...word };
+    this.wordMap = mapWord(this.word.value);
+  }
+
+  searchSegmentRight(syl, seg, props) {
+    const syllable = this.word.value[syl].value;
+    if (seg === null) {
+      seg = syllable.length - 1;
+    }
+    for (let i = seg - 1; i >= 0; i -= 1) {
+      if (props.matches(syllable[i])) {
+        return syllable[i];
+      }
+    }
+    if (syl > 0) {
+      return this.searchSegmentRight(syl - 1, null, props);
+    }
+    return null;
+  }
+
+  searchSegmentLeft(syl, seg, props) {
+    const syllable = this.word.value[syl].value;
+    if (seg === null) {
+      seg = 0;
+    }
+    for (let i = seg + 1; i < syllable.length; i += 1) {
+      if (props.matches(syllable[i])) {
+        return syllable[i];
+      }
+    }
+    if (syl < this.word.value.length - 1) {
+      return this.searchSegmentLeft(syl + 1, 0, props);
+    }
+    return null;
+  }
+
+  environment(
+    [syl, seg],
+    {
+      prevConsonant,
+      nextConsonant,
+      prevVowel,
+      nextVowel,
+      lastConsonant,
+      lastVowel,
+      suffix,
+      augmentation
+    }
+  ) {
+    return {
+      type: this.word.type,
+      meta: this.word.meta,
+      prevConsonant: prevConsonant && this.searchSegmentRight(syl, seg, consonantPred),
+      nextConsonant: nextConsonant && this.searchSegmentLeft(syl, seg, consonantPred),
+      prevVowel: prevVowel && this.searchSegmentRight(syl, seg, vowelPred),
+      nextVowel: nextVowel && this.searchSegmentRight(syl, seg, vowelPred),
+    };
   }
 
   wrap(results) {
+    if (!results || results.length === 0) {
+      return _handler => {};
+    }
     // TODO: inspect handler to determine its dependencies
     // assign dependencies to their dependents for change-listening
     // and finish everything
     return handler => results.forEach(handler);
   }
 
+  just(obj) {
+    const props = obj instanceof Props ? obj : new Props(obj);
+    return this.wrap(this.wordMap[obj.value]);
+  }
+
   segment(props) {
     props = props instanceof Props ? props : new Props(props);
     return this.wrap(
       Object.values(this.wordMap)
-        .map(indices => indices.filter(([syl, seg]) => props.matches(this.word[syl][seg])))
+        .map(indices => indices.filter(([syl, seg]) => props.matches(this.word.value[syl][seg])))
         .flat()
     );
   }
@@ -168,44 +235,46 @@ class Cap {
     return this.wrap(
       searchSpace
         .map(c => this.wordMap[c])
-        .map(indices => indices.filter(([syl, seg]) => meta.matches(this.word[syl][seg].meta)))
+        .map(indices => indices.filter(
+          ([syl, seg]) => meta.matches(this.word.value[syl][seg].meta))
+        )
         .flat()
     );
   }
 
   consonant(props) {
-    return this.segmentOfType(`consonant`, props);
+    return this.segmentOfType(objType.consonant, props);
   }
 
   vowel(props) {
-    return this.segmentOfType(`vowel`, props);
+    return this.segmentOfType(objType.vowel, props);
   }
 
   epenthetic(props) {
-    return this.segmentOfType(`epenthetic`, props);
+    return this.segmentOfType(objType.epenthetic, props);
   }
 
   suffix(props) {
-    return this.segmentOfType(`suffix`, props);
+    return this.segmentOfType(objType.suffix, props);
   }
 }
 
 const rules = [
-  [_.Fem, (abc, ctx, {previousVowel, previousConsonant}) => {
+  [_.c, (abc, ctx, {previousVowel, previousConsonant}) => {
     if (ctx.femA) {
-      return [abc.Fem.low];
+      return [abc.c.low];
     }
     if (previousConsonant.value === `r` && previousVowel.meta.intrinsic.ly.high && !previousVowel.meta.intrinsic.ly.rounded) {
-      return [abc.Fem.high];
+      return [abc.c.high];
     }
     if (previousConsonant.meta.intrinsic.ly.emphatic || previousConsonant.value === `r`) {
       // maybe emphatics can sometimes have -e idk
-      return [abc.Fem.low];
+      return [abc.c.low];
     }
     if (ctx.type === `participle`) {
-      return [abc.Fem.high, abc.Fem.low];
+      return [abc.c.high, abc.c.low];
     }
-    return [abc.Fem.high];
+    return [abc.c.high];
   }],
   [_.i, (abc, ctx, {previousConsonant, nextConsonant, codaConsonant}) => {
     // fuck this makes no sense
