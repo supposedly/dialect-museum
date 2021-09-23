eb6ef5dd0eeb3dc0d317e68e40d9b0481691d55d
hadi tarhini <hi@how.hadi.is>
2021-09-23 14:24:06 -0700

    Rewrite props for consistency & usability, rename to match

---
+++ src/backend/conversion/transformers/common/match.js
@@ -0,0 +1,95 @@
+/* eslint-disable max-classes-per-file */
+
+class Match {
+  constructor(_obj) {}
+
+  matches(_other) {
+    return false;
+  }
+}
+
+class MatchOne extends Match {
+  constructor(obj) {
+    super(obj);
+  
+    if (obj instanceof Match) {
+      this.matcher = obj.matches;
+    } else if (Array.isArray(obj)) {
+      this.matcher = new All(...obj).matches;
+    } else if (obj instanceof Function) {
+      this.matcher = obj;
+    } else if (obj instanceof Object) {
+      obj = Object.entries(obj).map(([k, v]) => [k, new MatchOne(v)]);
+      this.matcher = other => obj.every(([k, matcher]) => matcher.matches(other[k]));
+    } else {
+      this.matcher = other => obj === other;
+    }
+  }
+
+  matches(other) {
+    return this.matcher(other);
+  }
+}
+
+class Not extends MatchOne {
+  constructor(...objs) {
+    super(...objs);
+  }
+
+  matches(other) {
+    return !super.matches(other);
+  }
+}
+
+class MatchMultiple extends Match {
+  constructor(...objs) {
+    super(...objs);
+    this.objs = objs.map(obj => new MatchOne(obj));
+  }
+
+  matches(other) {
+    return this.objs.includes(other);
+  }
+}
+
+class Any extends MatchMultiple {
+  constructor(...objs) {
+    super(...objs);
+  }
+
+  matches(other) {
+    return this.objs.some(obj => obj.matches(other));
+  }
+}
+
+class None extends Any {
+  constructor(...objs) {
+    super(...objs);
+  }
+
+  matches(other) {
+    return !super.matches(other);
+  }
+}
+
+class All extends MatchMultiple {
+  constructor(...objs) {
+    super(...objs);
+  }
+
+  matches(other) {
+    return this.objs.every(obj => obj.matches(other));
+  }
+}
+
+export default Object.assign(
+  function match(obj) {
+    return new MatchOne(obj);
+  },
+  {
+    not(obj) { return new Not(obj); },
+    any(...objs) { return new Any(...objs); },
+    none(...objs) { return new None(...objs); },
+    all(...objs) { return new All(...objs); }
+  }
+)
