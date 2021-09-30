caf6071b4ec815f9f9511a9da50daf6d848d3e82
hadi tarhini <hi@how.hadi.is>
2021-09-30 12:10:49 -0700

    Rename all .js to .ts
    Lost all my uncommitted progress from yesterday because I didn't `git add` my .ts files and I didn't know that `git stash` doesn't stash untracked files without `-u` :(

--- src/backend/conversion/enums.js
+++
@@ -1,7 +0,0 @@
-// "fake enum"
-// takes an array of symbols and returns an object of [symbol, index]
-export function fenum(arr) {
-  const o = Object.fromEntries(arr.map((e, i) => [e, i]));
-  o.keys = arr;
-  return o;
-}
