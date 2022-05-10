a6efce5efb13fa4df6e88aef47c926559fa26bc9
hh <hi@how.hadi.is>
2022-05-09 18:04:12 -0700

    Roll own enums for stricter typing & so I can mess with their string values

--- src/backend/conversion/enums.ts
+++ src/backend/conversion/enums.ts
@@ -1,59 +1,97 @@
-export enum Articulator {
-  NULL = -1,
-  throat,
-  tongue,
-  lips,
+import {Union} from "ts-toolbelt";
+
+type DISTRIBUTE = any;
+
+type Trim<S extends string> = S extends ` ${infer Sub} ` | `${infer Sub} ` | ` ${infer Sub}` ? Trim<Sub> : S;
+type Join<Name extends string, Value extends string> = `` extends Name ? Value : `${Name}:${Value}`;
+type Entry<Name extends string, Line extends string> =
+  Line extends `${infer Key}=${infer Value}`
+    ? Record<Trim<Key>, Join<Name, Trim<Value>>>
+    : Record<Line, Join<Name, Line>>;
+
+export type Enum<Name extends string, S extends string> =
+  S extends `${' ' | '\n'}${infer Sub}` | `${infer Sub}${' ' | '\n'}`
+    ? Enum<Name, Sub>
+    : S extends `${infer A}\n${infer B}`
+      ? Union.Merge<Entry<Name, A> | Enum<Name, B>>
+      : `` extends S
+        ? {}
+        : Entry<Name, S>;
+
+export type EnumOf<Name extends string, Keys extends string> = Union.Merge<
+  Keys extends DISTRIBUTE ? Enum<Name, Keys> : never
+>;
+
+export type Of<O> = O[keyof O];
+
+function join(name: string, value: string): string {
+  return name.length ? `${name}:${value}` : value;
+}
+export function toEnum<Name extends string, S extends string>(name: Name, s: S): Enum<Name, S> {
+  return Object.fromEntries(
+    s.trim()
+      .replace(/\n/g, `,`)
+      .replace(/\s+/g, ``)
+      .split(`,`)
+      .map(m => {
+        if (m.includes(`=`)) {
+          const [key, value] = m.split(`=`);
+          return [key, join(name, value)];
+        }
+        return [m, join(name, m)];
+      }),
+  ) as Enum<Name, S>;
 }
 
-export enum Location {
-  NULL = -1,
-  glottis,
-  pharynx,
-  uvula,
-  velum,
-  palate,
-  bridge,
-  ridge,
-  teeth,
-  lips,
-}
+export const $Articulator = toEnum(`articulator`, `
+  throat
+  tongue
+  lips
+`);
+export type Articulator = typeof $Articulator;
 
-export enum Manner {
-  NULL = -1,
-  plosive,
-  fricative,
-  affricate,
-  approximant,
-  nasal,
-  flap,
-}
+export const $Location = toEnum(`location`, `
+  glottis
+  pharynx
+  uvula
+  velum
+  palate
+  bridge
+  ridge
+  teeth
+  lips
+`);
+export type Location = typeof $Location;
 
-export enum SegType {
-  NULL = -1,
-  suffix,
-  pronoun,
-  augmentation,
-  consonant,
-  vowel,
-  epenthetic,
-  modifier,
-  delimiter,
-}
+export const $Manner = toEnum(`manner`, `
+  plosive
+  fricative
+  affricate
+  approximant
+  nasal
+  flap
+`);
+export type Manner = typeof $Manner;
 
-export enum Ps {
-  first = 1,
-  second = 2,
+// These have to be nameless because I have some stupid code that
+// depends on each variant being exactly 1 character long
+export const $Ps = toEnum(``, `
+  first = 1
+  second = 2
   third = 3
-}
+`);
+export type Ps = typeof $Ps;
 
-export enum Nb {
-  singular = `s`,
-  dual = `d`,
-  plural = `p`
-}
+export const $Nb = toEnum(``, `
+  singular = s
+  dual = d
+  plural = p
+`);
+export type Nb = typeof $Nb;
 
-export enum Gn {
-  masc = `m`,
-  fem = `f`,
-  common = `c`
-}
+export const $Gn = toEnum(``, `
+  masc = m
+  fem = f
+  common = c
+`);
+export type Gn = typeof $Gn;
