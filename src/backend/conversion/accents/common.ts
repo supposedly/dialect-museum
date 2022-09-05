1b32a2cefff0779b2bcd7ced09f472d242f0dce0
hh <hi@how.hadi.is>
2022-09-05 13:30:17 -0700

    Add odds to accent features

--- src/backend/conversion/accents/common.ts
+++ src/backend/conversion/accents/common.ts
@@ -1,16 +1,21 @@
 import * as ABC from "../alphabets/common";
 import {Narrow as $} from "../utils/typetools";
 
-export type Layer<A extends ABC.AnyAlphabet, R extends Record<string, Set<string>>> = {
+export type Layer<A extends ABC.AnyAlphabet, R extends Record<string, Record<string, number> | Set<string>>> = {
   accents: R
 } & A;
 
 export type AnyLayer = {
-  accents: Record<string, Set<String>>
+  accents: Record<string, Record<string, number> | Set<string>>
 } & ABC.AnyAlphabet;
 
-type ToSets<R extends Record<string, string[]>> = {
-  [K in keyof R]: Set<R[K][number]>
+type ToSets<R extends Record<string, string[] | Record<string, number>>> = {
+  [K in keyof R]:
+    R[K] extends string[]
+      ? Set<R[K][number]>
+      : R[K] extends Record<string, number>  // fails without this (thinks there are other possibilities...)
+        ? R[K]
+        : never
 };
 
 export type AccentFeatures<A extends AnyLayer> = keyof A[`accents`];
@@ -21,7 +26,7 @@ export type AccentFeature<A extends AnyLayer, F extends AccentFeatures<A>> =
 
 export function toLayer<
   A extends ABC.AnyAlphabet,
-  R extends Record<string, string[]>,
+  R extends Record<string, string[] | Record<string, number>>,
 >(abc: A, accents: $<R>): Layer<A, ToSets<R>> {
   return {
     ...abc,
