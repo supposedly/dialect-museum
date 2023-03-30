d1d2ff5e5addc4f76b04855aec2fc7c67dcabc46
hh <hi@how.hadi.is>
2023-03-30 14:21:18 -0700

    Delete initializers to prep for making those transformers, finally remember to readd obj.ts

--- src/types.ts
+++
@@ -1,6 +0,0 @@
-import type {Obj} from "./backend/conversion/objects/obj";
-import type ObjType from "./backend/conversion/parsing/type";
-import type {Segment} from "./backend/conversion/symbols";
-
-export type Syllable = Obj<ObjType.syllable, Segment[]>;
-export type Word = Obj<ObjType.word, Syllable[]>;
