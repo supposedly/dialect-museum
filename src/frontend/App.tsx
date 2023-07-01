d36b96e43efc63b92cd58aab7b7a960c876b7df5
hh <hi@how.hadi.is>
2023-07-01 16:20:18 -0700

    Jank together a copy of the old interactive webpage w/ input

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,4 +1,4 @@
-import {createEffect, type Component} from 'solid-js';
+import {createEffect, type Component, createSignal, createRenderEffect, Setter} from 'solid-js';
 
 import {Parser, Grammar} from "nearley";
 import styles from './App.module.css';
@@ -7,23 +7,28 @@ import grammar from "../backend/conversion/parsing/grammar";
 
 const compiledGrammar = Grammar.fromCompiled(grammar);
 
+declare module "solid-js" {
+  namespace JSX {
+    // this sucks
+    interface Directives {
+      bind: [Accessor<string>, Setter<string>];
+    }
+  }
+}
+
+function bind(el: HTMLInputElement, accessor: typeof createSignal<string>) {
+  const [s, set] = accessor(``);
+  el.addEventListener(`input`, e => set((e.currentTarget as HTMLInputElement).value));
+  createRenderEffect(() => {
+    el.value = s();
+  });
+}
+
 const App: Component = () => {
-  console.log(new Parser(compiledGrammar).feed(`bruh`).results);
-  return <div class={styles.App}>
-    <header class={styles.header}>
-      {/* <img src={logo} class={styles.logo} alt="logo" /> */}
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        class={styles.link}
-        href="https://github.com/solidjs/solid"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn Solid
-      </a>
-    </header>
+  const [input, setInput] = createSignal(``);
+  return <div>
+    <input type="text" use:bind={[input, setInput]}></input>
+    <pre>{JSON.stringify(new Parser(compiledGrammar).feed(input()).results, null, 2)}</pre>
   </div>;
 };
 
