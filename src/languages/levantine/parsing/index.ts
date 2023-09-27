9c26d032c02186e38b7d7c7c8de7f57545cac0e2
hh <hi@how.hadi.is>
2023-09-27 14:51:11 -0700

    Remove parsing directory, need to reimplement later

--- src/languages/levantine/parsing/index.ts
+++
@@ -1,2 +0,0 @@
-export * as grammar from './grammar';
-export * as type from './type';
--- src/languages/levantine/parsing/parsing-symbols.ts
+++
@@ -1,7 +0,0 @@
-export * from '../layers/underlying/letters';
-export const symbol = {
-  emphatic: `*`,
-  stressed: `"`,
-  nasalized: `N`,
-  fus7a: `^`,
-};
