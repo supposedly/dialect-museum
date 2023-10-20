9132c03cce2851017d30bee0051a047c8765db24
hh <hi@how.hadi.is>
2023-10-20 00:34:55 -0700

    Attempt to implement f3vvl imperatives
    Do I handle geminate form-1 imperatives correctly...? What about higher forms that mock f3vl and don't want to have the 2i- inserted?

--- src/languages/levantine/rule-packs/lebanese/templates/verb/form-1/f3vl/imperative.ts
+++ src/languages/levantine/rule-packs/lebanese/templates/verb/form-1/f3vl/imperative.ts
@@ -1,5 +1,12 @@
 import ruleset from './base';
 import {letters} from '/languages/levantine/alphabets/underlying';
+import {separateContext} from '/lib/rules';
+
+const themeMap = {
+  a: `aa`,
+  i: `ee`,
+  u: `oo`,
+} as const;
 
 export default ruleset(
   {
@@ -12,7 +19,22 @@ export default ruleset(
   operations => ({
     prefix: {
       $i: [operations.preject(letters.plain.consonant.$, letters.plain.vowel.i)],  // preject doesn't invalidate a tracker node from being touched again
-      none: [],  // think i need this to circumvent default lol will see abt changing how that works
+      none: [],
     },
-  })
+    body: {
+      f3vvl: ({features: {root: $, theme}}) => [
+        separateContext($[0], `affected`),
+        separateContext($[1], `affected`),
+        letters.plain.vowel[themeMap[theme]],
+        separateContext($[2 as number], `affected`),
+      ],
+    },
+  }),
+  {
+    beforePronoun: {
+      target: {
+        env: ({before}, {vowel}) => before(vowel()),
+      },
+    },
+  }
 );
