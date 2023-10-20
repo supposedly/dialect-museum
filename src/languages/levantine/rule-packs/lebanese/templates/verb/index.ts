5a3646be6848d16a751a253398ff1bce132adf5f
hh <hi@how.hadi.is>
2023-10-20 09:57:30 -0700

    Organize verb forms in their own folder, add conjugation & indicative rules to verb export

--- src/languages/levantine/rule-packs/lebanese/templates/verb/index.ts
+++ src/languages/levantine/rule-packs/lebanese/templates/verb/index.ts
@@ -1,22 +1,27 @@
 import verb from './base';
 
-import form1 from './form-1';
-import fa33al from './fa33al';
-import tfa33al from './tfa33al';
-import stfa33al from './stfa33al';
-import faa3al from './faa3al';
-import tfaa3al from './tfaa3al';
-import stfaa3al from './stfaa3al';
-import af3al from './2af3al';
-import fta3vl from './fta3vl';
-import nfa3vl from './nfa3vl';
-import f3all from './f3all';
-import staf3al from './staf3al';
-import fa3laq from './fa3laq';
-import tfa3laq from './tfa3laq';
-import stfa3laq from './stfa3laq';
+import form1 from './forms/form-1';
+import fa33al from './forms/fa33al';
+import tfa33al from './forms/tfa33al';
+import stfa33al from './forms/stfa33al';
+import faa3al from './forms/faa3al';
+import tfaa3al from './forms/tfaa3al';
+import stfaa3al from './forms/stfaa3al';
+import af3al from './forms/2af3al';
+import fta3vl from './forms/fta3vl';
+import nfa3vl from './forms/nfa3vl';
+import f3all from './forms/f3all';
+import staf3al from './forms/staf3al';
+import fa3laq from './forms/fa3laq';
+import tfa3laq from './forms/tfa3laq';
+import stfa3laq from './forms/stfa3laq';
+
+import indicative from './indicative';
+import conjugation from './conjugation';
 
 export default verb.pack({
+  indicative,
+  conjugation,
   form1,
   fa33al,
   tfa33al,
