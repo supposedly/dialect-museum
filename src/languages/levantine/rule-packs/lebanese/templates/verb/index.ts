186585708fcf611a9340f6147d0c46a6c813538e
hh <hi@how.hadi.is>
2023-10-19 17:29:02 -0700

    Add new verb rulepacks to base verb's .pack()
    Jk now they're done

--- src/languages/levantine/rule-packs/lebanese/templates/verb/index.ts
+++ src/languages/levantine/rule-packs/lebanese/templates/verb/index.ts
@@ -3,13 +3,27 @@ import verb from './base';
 import form1 from './form-1';
 import fa33al from './fa33al';
 import tfa33al from './tfa33al';
+import stfa33al from './stfa33al';
 import faa3al from './faa3al';
 import tfaa3al from './tfaa3al';
+import stfaa3al from './stfaa3al';
+import af3al from './2af3al';
+import fta3vl from './fta3vl';
+import nfa3vl from './nfa3vl';
+import f3all from './f3all';
+import staf3al from './staf3al';
 
 export default verb.pack({
   form1,
   fa33al,
   tfa33al,
+  stfa33al,
   faa3al,
   tfaa3al,
+  stfaa3al,
+  af3al,
+  fta3vl,
+  nfa3vl,
+  f3all,
+  staf3al,
 });
